875e6d107f0adc96025bbbf70360ca979d5aba48
Patrik0097 <szabopatrik0411@gmail.com>
2024-06-11 21:54:12 +0200

    Add files via upload

--- bejelentkezes.php
+++ bejelentkezes.php
@@ -1,18 +1,42 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $felhasznalo = $_POST["felhasznalo"];
-    $jelszo = $_POST["jelszo"];
+//if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  //  $felhasznalo = $_POST["felhasznalo"];
+  //  $jelszo = $_POST["jelszo"];
 
-    // Ellenőrizd a felhasználó által megadott adatokat
-    // Például ellenőrizd a jelszót, majd bejelentkezheted a felhasználót
+  $servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "registration";
 
-    // Ide írd a bejelentkezési logikát
-    // ...
+// Csatlakozás létrehozása
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Példa: átirányítás a főoldalra
-    header("Location: index.php");
-    exit;
+// Csatlakozás ellenőrzése
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
+
+// Bejelentkezési adatok lekérdezése
+$sql = "SELECT username, password FROM users WHERE username = ?"; // Felhasználónév lekérdezése
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $_POST['username']); // 's' jelzi, hogy a paraméter egy string
+
+$stmt->execute();
+
+$result = $stmt->get_result();
+while ($row = $result->fetch_assoc()) {
+    if (password_verify($_POST['password'], $row['password'])) { // Jelszó ellenőrzése
+        echo "Sikeres bejelentkezés!";
+    } else {
+        echo "Hibás jelszó!";
+    }
+}
+
+$stmt->close();
+$conn->close();
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -34,16 +58,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <li><a href="feldolgozas.php">Megrendelés</a></li>
                 <li><a href="index.php" class="button">Regisztráció</a></li>
                 <li><a href="bejelentkezés.php" class="button">Bejelentkezés</a></li>
+                <div id="welcome">
+        Üdvözöljük, <?php echo $username; ?>!
+    </div>
             </ul>
         </nav>
     </header>
-    
-    <br>
-    <br>
-    <br>
     <br>
     <br>
     <h1>Bejelentkezés</h1>
+    <br>
     <form method="post" action="bejelentkezes.php">
         Felhasználónév: <input type="text" name="felhasznalo"><br>
         Jelszó: <input type="password" name="jelszo"><br>
--- kapcsoaltfeldolgozas.php
+++ kapcsoaltfeldolgozas.php
@@ -1,7 +1,7 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $uzenet = $_POST["uzenet"];
-    $cel_email = "szabopatrik0411@gmail.com"; // Ide írd be az e-mail címet
+    $cel_email = "szabopatrik0411@gmail.com"; // Cél e-amil
 
     // Elküldés
     mail($cel_email, "Kapcsolatfelvétel", $uzenet);
@@ -12,9 +12,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html>
 <head>
     <title>Kapcsolatfelvétel</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
+<header id="header" class="conatiner-fluid">
+        <h1><a href="index.html"><img src="images/SZPK_logo.webp" alt="SZPK"></a></h1>
+        <nav>
+            <ul>
+                <li><a href="index.html">Kezdőlap</a></li>
+                <li><a href="rólunk.html">Rólunk</a></li>
+                <li><a href="Torták.html">Torták</a></li>
+                <li><a href="feldolgozas.php">Megrendelés</a></li>
+                <li><a href="kapcsoaltfeldolgozas.php">Kapcsolat</a></li>
+                <li><a href="index.php" class="button">Regisztráció</a></li>
+                <li><a href="bejelentkezés.php" class="button">Bejelentkezés</a></li>
+            </ul>
+        </nav>
+    </header>
+    <br>
+    <br>
     <h1>Kapcsolatfelvétel</h1>
+    <br>
+    <br>
     <form method="post" action="kapcsolatfeldolgozas.php">
         Üzenet: <textarea name="uzenet" rows="4" cols="50"></textarea><br>
         <input type="submit" value="Küldés">
